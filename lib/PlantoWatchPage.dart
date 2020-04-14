5fab15a0ea2cb8a65c7683c696499203ff7a9df1
Arnab771 <arnabchakraborty771@gmail.com>
2020-04-14 11:37:11 +0530

    Changed onTap to onLongPress, added Scaffold

--- lib/PlantoWatchPage.dart
+++ lib/PlantoWatchPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'AddPage.dart';
+import 'CompletedPage.dart';
+import 'DroppedPage.dart';
+import 'WatchingPage.dart';
 import 'database/plantowatch.dart';
 import 'database/watching.dart';
 
@@ -33,7 +37,7 @@ class _PlantoWatchPageState extends State<PlantoWatchPage> {
                   borderRadius: BorderRadius.circular(14)),
               child: Card(
                 child: ListTile(
-                  onTap: () {
+                  onLongPress: () {
                     showDialog(
                         context: context,
                         barrierDismissible: false,
@@ -85,25 +89,105 @@ class _PlantoWatchPageState extends State<PlantoWatchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      key: _refreshKey,
-      onRefresh: refreshList,
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: FutureBuilder(
-            future: anime,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return view(snapshot.data);
-              }
-              if (snapshot.data == null || snapshot.data.length) {
-                return Text("Nothing here",
-                    style: TextStyle(fontSize: 20.0, color: Colors.grey));
-              }
-            },
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/icon.png",
+                      width: 150.0,
+                    ),
+                  ),
+                  Text(
+                    "Jiyu",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.arrow_upward),
+              title: Text("Watching"),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => WatchingPage())),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.check),
+              title: Text("Completed"),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => CompletedPage())),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text("Dropped"),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => DroppedPage())),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.note),
+              enabled: true,
+              title: Text("Plan to Watch"),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Plan to Watch"),
+      ),
+      body: RefreshIndicator(
+        key: _refreshKey,
+        onRefresh: refreshList,
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FutureBuilder(
+              future: anime,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return view(snapshot.data);
+                }
+                if (snapshot.data == null || snapshot.data.length) {
+                  return Text("Nothing here",
+                      style: TextStyle(fontSize: 20.0, color: Colors.grey));
+                }
+              },
+            ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              barrierDismissible: true,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Add"),
+                  content: AddPage(),
+                );
+              });
+        },
+        elevation: 6.0,
+        child: Icon(Icons.add),
       ),
     );
   }
