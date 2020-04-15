5d777caba4b8d2a3e83c8bdfbc5a5dc4a3cd8e58
Arnab771 <arnabchakraborty771@gmail.com>
2020-04-15 08:15:07 +0530

    Changed primarySwatch and canvasColor

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ import 'WatchingPage.dart';
 void main() => runApp(Jiyu());
 
 class Jiyu extends StatelessWidget {
+  final backgroundColor = Color(0xFF33325F);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Jiyu",
         theme: ThemeData(
-            primarySwatch: Colors.lightGreen, brightness: Brightness.dark),
+          primarySwatch: Colors.purple,
+          brightness: Brightness.dark,
+          canvasColor: backgroundColor,
+        ),
         home: WatchingPage());
   }
 }
