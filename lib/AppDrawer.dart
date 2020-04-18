6402824158916f63100d05f7b54cdfad68c3a5f1
Arnab771 <arnabchakraborty771@gmail.com>
2020-04-18 15:12:40 +0530

    ListTile now shows different color if selected

--- lib/AppDrawer.dart
+++ lib/AppDrawer.dart
@@ -60,57 +60,71 @@ class AppDrawer extends StatelessWidget {
                 ],
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.arrow_upward),
-              title: Text("Watching"),
-              onTap: () {
-                if (this._routeToWatching) {
-                  Navigator.pop(context);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => WatchingPage()));
-                }
-              },
+            Container(
+              color: this._routeToWatching == false ? Colors.purple[900] : null,
+              child: ListTile(
+                leading: Icon(Icons.arrow_upward),
+                title: Text("Watching"),
+                onTap: () {
+                  if (this._routeToWatching) {
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => WatchingPage()));
+                  }
+                },
+              ),
             ),
-            ListTile(
-                leading: Icon(Icons.check),
-                title: Text("Completed"),
-                onTap: () {
-                  if (this._routeToCompleted) {
-                    Navigator.pop(context);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                CompletedPage()));
-                  }
-                }),
-            ListTile(
-                leading: Icon(Icons.delete),
-                title: Text("Dropped"),
-                onTap: () {
-                  if (this._routeToDropped) {
-                    Navigator.pop(context);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => DroppedPage()));
-                  }
-                }),
-            ListTile(
-                leading: Icon(Icons.note),
-                title: Text("Plan to Watch"),
-                onTap: () {
-                  if (this._routeToPlanned) {
-                    Navigator.pop(context);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                PlantoWatchPage()));
-                  }
-                }),
+            Container(
+              color:
+                  this._routeToCompleted == false ? Colors.purple[900] : null,
+              child: ListTile(
+                  leading: Icon(Icons.check),
+                  title: Text("Completed"),
+                  onTap: () {
+                    if (this._routeToCompleted) {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  CompletedPage()));
+                    }
+                  }),
+            ),
+            Container(
+              color: this._routeToDropped == false ? Colors.purple[900] : null,
+              child: ListTile(
+                  leading: Icon(Icons.delete),
+                  title: Text("Dropped"),
+                  onTap: () {
+                    if (this._routeToDropped) {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  DroppedPage()));
+                    }
+                  }),
+            ),
+            Container(
+              color: this._routeToPlanned == false ? Colors.purple[900] : null,
+              child: ListTile(
+                  leading: Icon(Icons.note),
+                  title: Text("Plan to Watch"),
+                  onTap: () {
+                    if (this._routeToPlanned) {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  PlantoWatchPage()));
+                    }
+                  }),
+            ),
           ],
         ),
       ),
